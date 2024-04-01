18bfc5be4034f11ab774f9392911193e2edb53d0
FrayRyderX <frayryderx@gmail.com>
2024-04-01 00:25:50 -0500

    feat: add footer and navbar

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <div className="" style={{ backgroundColor: 'rgb(151,206,76)' }}>
+            <h1 className="text-3xl font-bold">Hello world!</h1>
+        </div>
+    )
+}
