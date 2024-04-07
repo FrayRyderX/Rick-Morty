7672efb98150324f2db361505cfa5becacb9ad80
FrayRyderX <frayrydex@gmail.com>
2024-04-06 23:52:32 -0500

    feat: test file

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,33 @@
 export const Home = () => {
     return (
-        <div className="bg-red-100">
-            <h1 className="text-3xl font-bold">Hello world!</h1>
+        <div className="bg-black h-screen p-5">
+            <div className="h-52 bg-red-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-green-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-red-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-red-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-green-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-red-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-red-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-green-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
+            <div className="h-52 bg-red-100">
+                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+            </div>
         </div>
     )
 }
