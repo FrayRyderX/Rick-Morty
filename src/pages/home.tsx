f90af9be99957d63514bcc1308d8ec1eb924200e
FrayRyderX <frayrydex@gmail.com>
2024-04-13 21:11:20 -0500

    feat: modificated page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,13 @@
 export const Home = () => {
     return (
-        <div className="bg-black h-screen p-5">
-            <div className="h-52 bg-red-100">
+        <div className="flex flex-col h-[calc(100vh-112px)] overflow-x-auto bg-black p-5 sm:h-[calc(100vh-76px)]">
+            <div className="flex-grow bg-red-100">
                 <h1 className="text-3xl font-bold text-white">Hello world!</h1>
             </div>
-            <div className="h-52 bg-green-100">
+            <div className="flex-grow bg-red-100 mt-3 mb-3">
                 <h1 className="text-3xl font-bold text-white">Hello world!</h1>
             </div>
-            <div className="h-52 bg-red-100">
-                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
-            </div>
-            <div className="h-52 bg-red-100">
-                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
-            </div>
-            <div className="h-52 bg-green-100">
-                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
-            </div>
-            <div className="h-52 bg-red-100">
-                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
-            </div>
-            <div className="h-52 bg-red-100">
-                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
-            </div>
-            <div className="h-52 bg-green-100">
-                <h1 className="text-3xl font-bold text-white">Hello world!</h1>
-            </div>
-            <div className="h-52 bg-red-100">
+            <div className="flex-grow bg-red-100">
                 <h1 className="text-3xl font-bold text-white">Hello world!</h1>
             </div>
         </div>
