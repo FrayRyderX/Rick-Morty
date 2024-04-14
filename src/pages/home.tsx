6bc10c62613f3bbfb09b434911de9a30db52331d
FrayRyderX <frayrydex@gmail.com>
2024-04-13 21:14:49 -0500

    style: delete overflow-x-auto

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,6 @@
 export const Home = () => {
     return (
-        <div className="flex flex-col h-[calc(100vh-112px)] overflow-x-auto bg-black p-5 sm:h-[calc(100vh-76px)]">
+        <div className="flex flex-col h-[calc(100vh-112px)] bg-black p-5 sm:h-[calc(100vh-76px)]">
             <div className="flex-grow bg-red-100">
                 <h1 className="text-3xl font-bold text-white">Hello world!</h1>
             </div>
