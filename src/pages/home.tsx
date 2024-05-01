388809a41058f5f9cf2913e5c67c9035579942c9
FrayRyderX <frayrydex@gmail.com>
2024-05-01 13:35:01 -0500

    feat: use react-router-dom

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 export const Home = () => {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -19,28 +20,34 @@ export const Home = () => {
     return (
         <div className="flex flex-col h-[calc(100vh-112px)] bg-black p-5 sm:h-[calc(100vh-76px)]">
             <div className="flex-grow grid justify-items-start p-2">
-                <button>
-                    <div className="md:flex-row md:items-center lg:flex lg:justify-between lg:items-center blue-background p-2 rounded-xl">
-                        <img className="pr-2 rounded-3xl" src="../images/pages/home/character.jpg" alt="Characters" width={imageSize} height={imageSize} />
-                        <h1 className="lg:text-4xl sm:text-xl text-center font-bold text-white hover:text-black">Characters</h1>
-                    </div>
-                </button>
+                <Link to="/characters">
+                    <button>
+                        <div className="md:flex-row md:items-center lg:flex lg:justify-between lg:items-center blue-background p-2 rounded-xl">
+                            <img className="pr-2 rounded-3xl" src="../images/pages/home/character.jpg" alt="Characters" width={imageSize} height={imageSize} />
+                            <h1 className="lg:text-4xl sm:text-xl text-center font-bold text-white hover:text-black">Characters</h1>
+                        </div>
+                    </button>
+                </Link>
             </div>
             <div className="flex-grow grid justify-items-center mt-3 mb-3 p-2">
-                <button>
-                    <div className="md:flex-row md:items-center lg:flex lg:justify-between lg:items-center blue-background p-2 rounded-xl">
-                        <img className="pr-2 rounded-3xl" src="../images/pages/home/episode.jpg" alt="Locations" width={imageSize} height={imageSize} />
-                        <h1 className="lg:text-4xl sm:text-xl text-center font-bold text-white hover:text-black">Locations</h1>
-                    </div>
-                </button>
+                <Link to="/locations">
+                    <button>
+                        <div className="md:flex-row md:items-center lg:flex lg:justify-between lg:items-center blue-background p-2 rounded-xl">
+                            <img className="pr-2 rounded-3xl" src="../images/pages/home/episode.jpg" alt="Locations" width={imageSize} height={imageSize} />
+                            <h1 className="lg:text-4xl sm:text-xl text-center font-bold text-white hover:text-black">Locations</h1>
+                        </div>
+                    </button>
+                </Link>
             </div>
             <div className="flex-grow grid justify-items-end p-2">
-                <button>
-                    <div className="md:flex-row md:items-center lg:flex lg:justify-between lg:items-center blue-background p-2 rounded-xl">
-                        <img className="pr-2 rounded-3xl" src="../images/pages/home/locations.jpg" alt="Episodes" width={imageSize} height={imageSize} />
-                        <h1 className="lg:text-4xl sm:text-xl text-center font-bold text-white hover:text-black">Episodes</h1>
-                    </div>
-                </button>
+                <Link to="/episodes">
+                    <button className='hover:text-black'>
+                        <div className="md:flex-row md:items-center lg:flex lg:justify-between lg:items-center blue-background p-2 rounded-xl">
+                            <img className="pr-2 rounded-3xl" src="../images/pages/home/locations.jpg" alt="Episodes" width={imageSize} height={imageSize} />
+                            <h1 className="lg:text-4xl sm:text-xl text-center font-bold text-white hover:text-black">Episodes</h1>
+                        </div>
+                    </button>
+                </Link>
             </div>
         </div>
     )
