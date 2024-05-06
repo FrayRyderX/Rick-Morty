4dc0f32613db545f685abe82fbb7fdd9fddeaecc
FrayRyderX <frayrydex@gmail.com>
2024-05-05 23:19:01 -0500

    feat: create interface and update page

--- src/model/characters/pages/character.tsx
+++ src/model/characters/pages/character.tsx
@@ -1,45 +1,9 @@
 import { useState, useEffect } from 'react';
 import { instance } from '../../../api';
-
-interface Result {
-    created: Date;
-    episode: string[];
-    gender: Gender;
-    id: number;
-    image: string;
-    location: Location;
-    name: string;
-    origin: Location;
-    species: Species;
-    status: Status;
-    type: string;
-    url: string;
-}
-
-enum Gender {
-    Female = "Female",
-    Male = "Male",
-    Unknown = "unknown",
-}
-
-interface Location {
-    name: string;
-    url: string;
-}
-
-enum Species {
-    Alien = "Alien",
-    Human = "Human",
-}
-
-enum Status {
-    Alive = "Alive",
-    Dead = "Dead",
-    Unknown = "unknown",
-}
+import { Characters } from '../utils/interface/interface';
 
 export const Character = () => {
-    const [characters, setCharacters] = useState<Result[]>([]);
+    const [characters, setCharacters] = useState<Characters[]>([]);
     const [, setError] = useState(null);
 
     useEffect(() => {
