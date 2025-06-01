070bf668ce76828ec6431d7183379c5dc4fd6f0f
FrayRyderX <frayryderx@gmail.com>
2025-06-01 18:37:27 -0500

    fix: update imports!

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import { Card } from './card/card';
-import { Navbar } from './navbar/navbar';
-import { screenInformative } from './globalComponents';
-
-export {
-  Card,
-  Navbar,
-  screenInformative
-};
