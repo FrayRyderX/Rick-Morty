dd5042e274afa3cf692469fb9768e03acfe582db
FrayRyderX <frayryderx@gmail.com>
2025-06-01 18:37:54 -0500

    feat: add loading!

--- src/components/globalComponents.tsx
+++ src/components/globalComponents.tsx
@@ -6,4 +6,15 @@ export const screenInformative = (message: string) => {
 			</div>
 		</div>
 	)
-}
+};
+
+export const loadingScreen = () => {
+	return (
+		<div className="bg-black h-[calc(100vh-76px)] p-5 sm:h-[calc(100vh-76px)]">
+			<div className="rounded-3xl blue-background h-[calc(100vh-115px)] sm:h-[calc(100vh-115px)] flex flex-col justify-center items-center gap-6">
+				<div className="w-20 h-20 border-8 border-black border-t-transparent rounded-full animate-spin"></div>
+				<div className="text-black text-4xl sm:text-4xl md:text-6xl lg:text-6xl xl:text-6xl">Cargando personajes...</div>
+			</div>
+		</div>
+	)
+};
