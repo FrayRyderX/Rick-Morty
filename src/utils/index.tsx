6c59f4d180b04aecf248a6cd2631370409ab18b0
FrayRyderX <frayryderx@gmail.com>
2025-06-01 18:33:42 -0500

    style: update variable name and add delay function

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,8 @@
-export const formatedDate = (created: Date) => {
+export const formattedDate = (created: Date) => {
   const data = created.toLocaleString().split("T");
   return data[0];
+};
+
+export const delay = (time: number) => {
+  return new Promise(resolve => setTimeout(resolve, time));
 };
